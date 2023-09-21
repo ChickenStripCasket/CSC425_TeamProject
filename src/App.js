c840dc53ce2dba2241773c65860b2eda99ac1f9f
boluakindele1 <120821391+boluakindele1@users.noreply.github.com>
2023-09-21 13:32:48 -0500

    y

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <main>
+      <Header />
+      <header />
       {/* Header */}
       <header className='header'></header>
       {/* Toolbar */}
