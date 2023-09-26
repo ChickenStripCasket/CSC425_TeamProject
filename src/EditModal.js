261bee3febb937b7b9f7fdc3079a09973dfc1d34
Duosion <duosiont@gmail.com>
2023-09-26 14:18:27 -0500

    Fixes.

--- src/EditModal.js
+++ src/EditModal.js
@@ -1,15 +1,24 @@
 import './EditModal.css'
 import Modal from './Modal'
+import TextInput from './TitleTextInput'
+import { useState } from "react"
+import { IconButton } from './material/IconButton'
 
 export default function EditModal({
     editTask,
     onClose
 }) {
+    const [inputValue, setInputValue] = useState(editTask?.title)
+
     return (
         <Modal visible={editTask} onClose={onClose}>
-            <header>
+            <header className='modal-header-container'>
+                <h3 className='title-large modal-header' >Edit Task</h3>
+                <IconButton icon="delete"/>
+                <IconButton icon="close" onClick={onClose}/>
 
             </header>
+            <TextInput title='Title' placeHolder={'Enter a Title...'} value= {inputValue} onValueChanged={(value)=>{setInputValue(value)}}></TextInput>
         </Modal>
     )
 }
--- src/Header.js
+++ src/Header.js
@@ -1,17 +1,15 @@
 import React from 'react'
 import'./header.css'
+import { FilledButton } from './material/FilledButton';
 
 
 function Header () {
 
     return (
-        <div >
-        <header className="header">
-            <h1>Tasks</h1>
-            <button className="newbtn">New +</button>
+        <header className='header'>
+            <h1 className='display-large'>Tasks</h1>
+            <FilledButton label="New" icon="add"/>
         </header>
-        </div>
-
     );
 
 }
